25de20bd1f5f631d18e4a0bd04ef0773c0c2af45
rjjenkins4 <rjjenkins4@gmail.com>
2023-04-24 16:45:10 -0400

    Initial commit

---
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.google.codelabs.mdc.kotlin.shrine
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
+import kotlinx.android.synthetic.main.shr_signup_login_fragment.view.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        // Set an error if the password is less than 8 characters.
+        view.button3.setOnClickListener {
+            (activity as NavigationHost).navigateTo(
+                ProductGridFragment(),
+                true
+            ) // Navigate to the next Fragment
+        }
+
+        return view
+    }
+}
--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -6,12 +6,15 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
-import com.google.codelabs.mdc.kotlin.shrine.staggeredgridlayout.StaggeredProductCardRecyclerViewAdapter
 import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
@@ -22,28 +25,15 @@ class ProductGridFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment with the ProductGrid theme
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
 
-        // Set up the tool bar
+        // Set up the toolbar.
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
 
         // Set up the RecyclerView
-        view.recycler_view.setHasFixedSize(true)
-        val gridLayoutManager = GridLayoutManager(context, 2, RecyclerView.HORIZONTAL, false)
-        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-            override fun getSpanSize(position: Int): Int {
-                return if (position % 3 == 2) 2 else 1
-            }
-        }
-        view.recycler_view.layoutManager = gridLayoutManager
-        val adapter = StaggeredProductCardRecyclerViewAdapter(
-                ProductEntry.initProductEntryList(resources))
-        view.recycler_view.adapter = adapter
-        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_large)
-        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_small)
-        view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
 
         return view;
     }
